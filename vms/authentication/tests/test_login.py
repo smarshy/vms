ca9bca0e338d179e1367e0c5a3f02bf046a14452
Vatsala <vatsala.swaroop@gmail.com>
2016-08-12 14:09:55 +0530

    auth

--- vms/authentication/tests/test_login.py
+++ vms/authentication/tests/test_login.py
@@ -45,7 +45,8 @@ class TestAccessControl(LiveServerTestCase):
         cls.driver.quit()
         super(TestAccessControl, cls).tearDownClass()
 
-    def login(self, credentials):
+    //segregate - var - authentiction
+	def login(self, credentials):
         self.driver.get(self.live_server_url + self.authentication_page)
         self.driver.find_element_by_id(self.login_id).send_keys(credentials['username'])
         self.driver.find_element_by_id(self.login_password).send_keys(credentials['password'])
@@ -55,23 +56,29 @@ class TestAccessControl(LiveServerTestCase):
         self.driver.find_element_by_link_text('Log Out').click()
 
     def test_authentication_page(self):
-        self.driver.get(self.live_server_url + self.homepage)
-        self.driver.find_element_by_link_text('Log In').click()
+		page = self.page
+        page.get(self.live_server_url + self.homepage)
+		page.login()
+        #self.driver.find_element_by_link_text('Log In').click()
         self.assertEqual(self.driver.current_url, self.live_server_url +
                 self.authentication_page)
-
-    def test_correct_admin_credentials(self):
+		
+	def test_correct_admin_credentials(self):
         '''
         Method to simulate logging in of a valid admin user and check if they
         redirected to '/home' and no errors are generated.
         '''
         self.login({ 'username' : 'admin', 'password' : 'admin'})
-        self.assertEqual(self.driver.current_url, self.live_server_url +
+		
+		# workaround page.get_homepage
+		
+		self.assertEqual(self.driver.current_url, self.live_server_url +
                 self.homepage)
 
         with self.assertRaises(NoSuchElementException):
-            self.driver.find_element_by_class_name(self.incorrect_login_error)
-        self.logout()
+            page.get_incorrect_login_error()
+			#self.driver.find_element_by_class_name(self.incorrect_login_error)
+        page.logout()
 
     def test_incorrect_admin_credentials(self):
         '''
@@ -79,14 +86,14 @@ class TestAccessControl(LiveServerTestCase):
         they are displayed an error and redirected to login page again.
         '''
         self.login({ 'username' : 'admin', 'password' : 'wrong_password'})
-        self.assertNotEqual(self.driver.current_url, self.live_server_url +
+        # subst home
+		self.assertNotEqual(self.driver.current_url, self.live_server_url +
                 self.homepage)
 
         self.assertEqual(self.driver.current_url, self.live_server_url +
-                self.authentication_page)
+                page.authentication_page)
 
-        self.assertNotEqual(self.driver.find_element_by_class_name(
-            self.incorrect_login_error), None)
+        self.assertNotEqual(page.get_incorrect_login_error(), None)
 
     def test_correct_volunteer_credentials(self):
         '''
@@ -94,12 +101,13 @@ class TestAccessControl(LiveServerTestCase):
         they are redirected to '/home'
         '''
         self.login({ 'username' : 'volunteer', 'password' : 'volunteer'})
-        self.assertEqual(self.driver.current_url, self.live_server_url +
+        #
+		self.assertEqual(self.driver.current_url, self.live_server_url +
                 self.homepage)
 
         with self.assertRaises(NoSuchElementException):
-            self.driver.find_element_by_class_name(self.incorrect_login_error)
-        self.logout()
+            page.get_incorrect_login_error()
+        page.logout()
 
     def test_incorrect_volunteer_credentials(self):
         '''
@@ -108,11 +116,11 @@ class TestAccessControl(LiveServerTestCase):
         '''
         self.login({ 'username' : 'volunteer', 'password' : 'wrong_password'})
         
-        self.assertNotEqual(self.driver.current_url, self.live_server_url +
+        # 
+		self.assertNotEqual(self.driver.current_url, self.live_server_url +
                 self.homepage)
 
         self.assertEqual(self.driver.current_url, self.live_server_url +
-                self.authentication_page)
+                page.authentication_page)
 
-        self.assertNotEqual(self.driver.find_element_by_class_name(
-            self.incorrect_login_error), None)
+        self.assertNotEqual(page.get_incorrect_login_error(), None)
